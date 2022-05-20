f5814b9310faa9c61864b5dc1d1db88195977363
shubham <shubhamrawatktw@gmail.com>
2022-05-20 17:35:28 +0530

    fetched news and displayed and remove func done

---
+++ src/context.js
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useReducer } from "react";
+
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions";
+import reducer from "./reducer";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
+
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING });
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+const removeStory = (id)=>{
+dispatch({type:REMOVE_STORY,payload:id})
+}
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state ,removeStory}}>{children}</AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ src/reducer.js
@@ -0,0 +1,31 @@
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true };
+
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      };
+    case REMOVE_STORY:
+      return {
+        ...state,
+        hits:state.hits.filter((story)=> story.objectID !== action.payload)
+      };
+
+    default:
+      throw new Error(`no matching ${action.type} action type`);
+  }
+};
+export default reducer;
